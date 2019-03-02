0da7671fea553ebae8c649a7cdd66f961a647a1c
nattkins <neville.attkins@gmail.com>
2019-03-02 21:50:15 +0000

    added setup to run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
-apt-install() {
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get update -q
-	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
-	return $?
-}
+# APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
+# apt-install() {
+# 	export DEBIAN_FRONTEND=noninteractive
+# 	apt-get update -q
+# 	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
+# 	return $?
+# }
 
-#install ffmpeg to container
-add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
-apt-install || exit 1
+# #install ffmpeg to container
+# add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
+# apt-install || exit 1
+#! /bin/bash
 
-#create folders
 mkdir data
-mkdir data/bin
+cd data
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+mkdir bin
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
+
+# #create folders
+# mkdir data
+# mkdir data/bin
 
 #run style transfer on video
 # python transform_video.py --in-path examples/content/fox.mp4 \
