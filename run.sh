b528b418669dc62dbad8ad62db0e477a66810438
nattkins <neville.attkins@gmail.com>
2019-03-03 22:12:10 +0000

    revert

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-APT_PACKAGES="apt-utils wget ffmpeg libav-tools x264 x265"
+APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
 apt-install() {
 	export DEBIAN_FRONTEND=noninteractive
 	apt-get update -q
 	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
 	return $?
 }
-
-
 
 # #install ffmpeg to container
 # add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
@@ -24,14 +22,6 @@ apt-install() {
 #   --out-path /artifacts/out.mp4 \
 #   --device /gpu:0 \
 #   --batch-size 4 2>&1
-
-mkdir data
-cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-mkdir bin
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-unzip train2014.zip
-cd
 
 python style.py --style examples/style/renoirP.jpg \
   --checkpoint-dir examples/style \
--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 mkdir data
 cd data
-python wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
-python wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-tar train2014.zip
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
 
