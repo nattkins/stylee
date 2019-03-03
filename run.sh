ceb6906a07e6e574e5f30aebad39eeac92f219bc
nattkins <neville.attkins@gmail.com>
2019-03-03 11:21:17 +0000

    again

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265 wget"
+APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
 apt-install() {
 	export DEBIAN_FRONTEND=noninteractive
 	apt-get update -q
 	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
 	return $?
 }
-
+app-get install wget
 
 
 # #install ffmpeg to container
@@ -30,7 +30,7 @@ cd data
 wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-tar train2014.zip
+unzip train2014.zip
 cd
 
 python style.py --style examples/style/renoirP.jpg \
--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-wget=/usr/bin/wget
 
 mkdir data
 cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+python wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-unzip train2014.zip
+python wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+tar train2014.zip
 
-sh ././run.sh
